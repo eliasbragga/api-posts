27ebe85e83eed3c72118728d028254a67ad1abcd
eliasbragga-pessoal <eliasbragga44@gmail.com>
2025-06-27 23:32:45 -0400

    config cors

--- src/main/java/com/example/apiposts/controller/PostController.java
+++ src/main/java/com/example/apiposts/controller/PostController.java
@@ -5,6 +5,7 @@ import com.example.apiposts.service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,6 +16,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/posts")
+@CrossOrigin(origins = "*")
 public class PostController {
 
     @Autowired
